24349e0739d143ef07430996275aeca5b17e5fc0
Tiago Nascimento <xtheosirian@gmail.com>
2014-08-31 21:42:00 -0300

    update lista06

--- cptN06.c
+++ cptN06.c
@@ -14,7 +14,7 @@ void ex1() {
 		printf("Media dos numeros entre %d e %d = %.2f", num1, num2, media);
 	} else if (num1 > num2) {
 		int count = (num1 - num2) / 2;
-		if (num1 % 2 == 0 || num2 % == 0) count++;
+		if (num1 % 2 == 0 || num2 % 2 == 0) count++;
 		printf("Quantidade de numeros pares entre %d e %d = %d", num2, num1, count);
 	} else {
 		printf("Voce informou dois numeros iguais.");
@@ -25,7 +25,7 @@ void ex2() {
 	int prime(int n) {
 		if (n < 3) return n > 1;
 		if (n % 2 == 0 || n % 3 == 0) return 1;
-		for (int i = 5, l = sqrt(n); i < l; i += 6){
+		for (int i = 5, l = sqrt(n); i < l; i += 6) {
 			if (n % i == 0 || n % (i + 2) == 0) return 0;
 		}
 		return 1;
@@ -57,17 +57,21 @@ void ex3() {
 	float mediaM = soma / mulheres;
 	printf("Homens:\t\t%d", homens);
 	printf("Mulheres:\t\t%d", mulheres);
-	printf("Maior Altura:\t\t%d S=%c", altura[maior], sexo[maior]);
-	printf("Menor Altura:\t\t%d S=%c", altura[menor], sexo[menor]);
-	printf("Media Mulheres: %d", mediaM);
+	printf("Maior Altura:\t\t%.2f S=%c", altura[maior], sexo[maior]);
+	printf("Menor Altura:\t\t%.2f S=%c", altura[menor], sexo[menor]);
+	printf("Media Mulheres: %.2f", mediaM);
 }
 
 void ex4() {
-	float ctof(float c) return (c + 32) * (9. / 5);
-	float ftoc(float f) return (f - 32) * (5. / 9);
-	printf("|%12s\t|\t%-12s|", "CELSIUS", "FAHRENHEIT");
-	for (int i = 40; i < 70; i++) {
-		printf("|%10d\t|\t%-10.2f|", i, ctof(i));
+	float ctof(float c) {
+		return (c + 32) * (9. / 5);
+	}
+	float ftoc(float f) {
+		return (f - 32) * (5. / 9);
+	}
+	printf("|%12s|%12s  ||  %12s|%12s|\n", "CELSIUS", "FAHRENHEIT", "FAHRENHEIT", "CELSIUS");
+	for (float i = 40; i < 70; i++) {
+		printf("|%12.2f|%12.2f  ||  %12.2f|%12.2f|\n", i, ctof(i), i, ftoc(i));
 	}
 }
 
@@ -76,15 +80,15 @@ void ex5() {
 }
 
 int main() {
-	print("LISTA DE EXERCICIOS DE FDA - 05");
+	printf("LISTA DE EXERCICIOS DE FDA - 06\n");
 
-	srand(time(NULL))
+	srand(time(NULL));
 
-	ex1();
-	ex2();
-	ex3();
+	//ex1();
+	//ex2();
+	//ex3();
 	ex4();
-	ex5();
+	//ex5();
 
 	getchar();
 	getchar();
