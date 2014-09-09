cf1fb3b43ad4989ff28d96492a1a749e1746b914
Tiago Nascimento <theosirian@users.noreply.github.com>
2014-09-08 23:01:08 -0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,32 +2,32 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+TAGS: PRINT, ARRAY, VETOR
+COMO USAR:
+    - Mudar o tipo do vetor nos argumentos.
+    - Mudar os printfs para o tipo correto.
+    - Usar.
+EXEMPLOS:
+    - String:
+        -Arg = "printArray(char * a[], int size)"
+        -Printf = "....printf("%s"...."
+    - Int:
+        -Arg = "printArray(int a[], int size)"
+        -Printf = "....printf("%d"...."
+    - Float:
+        -Arg = "printArray(float a[], int size)"
+        -Printf = "....printf("%.2f"...."
+FORMATO DE OUTPUT:
+    - {0, 1, 2, 3, 4, 5, 6} -> [0, 1, 2, 3, 4, 5, 6]
+    - {"hello", "world", "this", "is", "your", "captain"} -> [hello, world, this, is, your, captain]
+    - {0.6, 0.9, 1.2, 1.5} -> [0.60, 0.90, 1.20, 1.50]
+EXTRA:
+    - Voce pode ainda customizar:
+        - {}, (), <>, ao inves de []
+        - Elementos aparecerem entre aspas: ..."%s"... -> ..."\"%s\""...
+ */
 void printArray(char * a[], int size) {
-	/*
-	TAGS: PRINT, ARRAY, VETOR
-	COMO USAR:
-	    - Mudar o tipo do vetor nos argumentos.
-	    - Mudar os printfs para o tipo correto.
-	    - Usar.
-	EXEMPLOS:
-	    - String:
-	        -Arg = "printArray(char * a[], int size)"
-	        -Printf = "....printf("%s"...."
-	    - Int:
-	        -Arg = "printArray(int a[], int size)"
-	        -Printf = "....printf("%d"...."
-	    - Float:
-	        -Arg = "printArray(float a[], int size)"
-	        -Printf = "....printf("%.2f"...."
-	FORMATO DE OUTPUT:
-	    - {0, 1, 2, 3, 4, 5, 6} -> [0, 1, 2, 3, 4, 5, 6]
-	    - {"hello", "world", "this", "is", "your", "captain"} -> [hello, world, this, is, your, captain]
-	    - {0.6, 0.9, 1.2, 1.5} -> [0.60, 0.90, 1.20, 1.50]
-	EXTRA:
-	    - Voce pode ainda customizar:
-	        - {}, (), <>, ao inves de []
-	        - Elementos aparecerem entre aspas: ..."%s"... -> ..."\"%s\""...
-	 */
 	if (size > 0) {
 		printf("\n[");
 		printf("%s", a[0]);
@@ -37,12 +37,12 @@ void printArray(char * a[], int size) {
 	} else printf("\n[]\n");
 }
 
+/*
+TAGS: FATORIAL, RECURSAO
+COMO FUNCIONA:
+    - Se o n for menor que 2 (0 ou 1) o fatorial e 1.
+    - Se nao, o fatorial e n multiplicado por n-1.
+ */
 void fact(int n) {
-	/*
-	TAGS: FATORIAL, RECURSAO
-	COMO FUNCIONA:
-	    - Se o n for menor que 2 (0 ou 1) o fatorial e 1.
-	    - Se nao, o fatorial e n multiplicado por n-1.
-	 */
 	return n < 2 ? 1 : n * fact(n - 1);
 }
