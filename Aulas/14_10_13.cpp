db3a500e807923949571339d3c41a0f9040327ef
Gabriel Laube <gabriel.laube@hotmail.com>
2014-10-14 15:59:39 -0300

    Update 14_10_13.cpp

--- Aulas/14_10_13.cpp
+++ Aulas/14_10_13.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 struct produto{
     char name[65];
     int code;
@@ -16,7 +15,7 @@ struct pessoa{
     int idade;
 };
 
-int leitura(struct pessoa *o){
+int leitura(struct pessoa *o,int *maior,int *f1835){
     puts("Informe a cor do cabelo(C-castanho, L-louros, P-pretos):");
     scanf(" %c",&(*o).cabelo);
     puts("Informe a cor dos olhos(C-castanho,A=azuis):");
@@ -27,6 +26,8 @@ int leitura(struct pessoa *o){
         puts("Informe a idade:");
         scanf("%d",&(*o).idade);
     }while((*o).idade<=0);
+    if(!*maior||*maior<(*o).idade)*maior=(*o).idade;
+    if(((*o).sexo=='f'||(*o).sexo=='F')&&((*o).idade>17&&(*o).idade<36))*f1835+=1;//notação *int++ não é valida com ponteiros
     return (((*o).olhos=='c')||((*o).olhos=='C'))&&(((*o).cabelo=='P')||((*o).cabelo=='p'));
 }
 
@@ -36,14 +37,12 @@ void mostrarCampos(struct produto o){
     printf("\n%c",o.size);
     printf("\n%.2f\n",o.price);
 }
-
 void mostrarCamposex2(struct pessoa o){
     printf("\n%c",o.cabelo);
     printf("\n%c",o.olhos);
     printf("\n%c",o.sexo);
     printf("\n%i\n",o.idade);
 }
-
 
 void ex1(){
     struct produto p[4];
@@ -74,19 +73,20 @@ void ex1(){
 }
 
 void ex2(){
-    int n,count=0;
-
+    int n,count=0,maior,f1835=0;
     do{
         puts("Informe o numero de pessoas:");
         scanf("%d",&n);
     }while(n<=0);
     struct pessoa p[n];
     for(int x=0;x<n;x++){
-        if(leitura(&p[x]))count++;
+        if(leitura(&p[x],&maior,&f1835))count++;
     }
     for(int x=0;x<n;x++){
         mostrarCamposex2(p[x]);
     }
+    printf("\nmaior idade: %d\n",maior);
+    printf("%d mulheres com idade entre 18 e 35\n",f1835);
     printf("%.2f %% das pessoas tem cabelo preto e olhos castanhos",!count?0:count/(float)n*100);
 }
 
@@ -96,14 +96,13 @@ int main(){
         puts("Informe o numero do exercicio 1 ou 2:");
         scanf("%d",&pick);
     }while(pick<1||pick>2);
-
     switch(pick){
-    case 1:
-        ex1();
-        break;
-    case 2:
-        ex2();
-        break;
+        case 1:
+            ex1();
+            break;
+        case 2:
+            ex2();
+            break;
     }
     return 0;
 }
