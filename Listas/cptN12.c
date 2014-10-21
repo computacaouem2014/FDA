9f0ef91651f2e95eba2de8bb2d0772b37a88462f
MurkyDTM <diogomurat@gmail.com>
2014-10-21 13:03:59 -0200

    tudo bay giu

--- Listas/cptN12.c
+++ Listas/cptN12.c
@@ -64,6 +64,53 @@ void printMatrixI(int m, int n, int arr[m][n]){
     }
 }
 
+void printMatrixC(int m, int n, char arr[m][n]){
+    if (m > 1){
+        printf("\n⎡");
+        for (int j = 0; j < n; j++){
+            printf("%2c ", arr[0][j]);
+        }
+        printf("⎤\n");
+        printf("⎢");
+        for (int k = 0; k < n; k++){
+            printf("   ");
+        }
+        printf("⎥\n");
+        for (int i = 1; i < m-1; i++){
+            if (i != 1){
+                printf("⎢");
+                for (int k = 0; k < n; k++){
+                    printf("   ");
+                }
+                printf("⎥\n");
+            }
+            printf("⎢");
+            for (int j = 0; j < n; j++){
+                printf("%2c ", arr[i][j]);
+            }
+            printf("⎥\n");
+        }
+        printf("⎢");
+        for (int k = 0; k < n; k++){
+            printf("   ");
+        }
+        printf("⎥\n");
+        printf("⎣");
+        for (int j = 0; j < n; j++){
+            printf("%2c ", arr[m-1][j]);
+        }
+        printf("⎦\n\n");
+    } else if (m == 1){
+        printf("\n[");
+        for (int i = 0; i < n; i++){
+                printf("%2c ", arr[0][i]);
+        }
+        printf("]\n\n");
+    } else {
+        printf("\n[]\n\n");
+    }
+}
+
 void printMatrixF(int m, int n, float arr[m][n]){
     if (m*n > n){
         printf("\n⎡");
@@ -202,7 +249,10 @@ void ex4(){
 		}
 	}
 	printMatrixI(m, n, a);
-	
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if ((i + j) % 2 != 0)
+				printf("A[%d][%d] = %d\n", i+1, j+1, a[i][j]);
 }
 
 void ex5(){
@@ -302,22 +352,97 @@ void ex8(){
 	printf("Minimax: %d", minimax);
 }
 
-void ex9(){
-	int m = 3, n = 3;
-	int a[m][n];
-	for (int i = 0; i < m; i++){
-		for (int j = 0; j < n; j++){
-//			printf("Informe o elemento A[%d][%d]: ", i+1, j+1);
-//			scanf("%d", a[i][j]);
-			a[i][j] = rand() % 20;
+char* toUpperCase(int size, char *str){
+	char *str2 = malloc((size + 1) * sizeof(char));
+	str2[size] = '\0';
+	for (int i = 0; i < size; i++){		
+		if (str[i] > 96 && str[i] < 123){
+			str2[i] = str[i] - 32;
 		}
 	}
-	printMatrixI(m, n, a);
+	return str2;
+}
+
+void ex9(){
+	int m = 3;
+	char a[m][m];
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < m; j++){
+//			printf("Informe o elemento A[%d][%d]: ", i+1, j+1);
+//			scanf("%d", a[i][j]);
+			a[i][j] = rand() % 25 + 65;
+		}
+	}
+	printMatrixC(m, m, a);
+	char *s = malloc((m + 1) * sizeof(char));
+	printf("Informe uma string de %d caracteres: ", m);
+	fgets(s, m + 1, stdin);
+	strtok(s, "\n");
+	s = toUpperCase(m, s);
+	for (int i = 0; i < m; i++){
+		char *str = malloc(m * sizeof(char));
+		char *str2 = malloc(m * sizeof(char));
+		int j;
+		for (j = 0; j < m; j++){
+			str[j] = a[i][j];
+			str2[j] = a[j][i];
+		}
+		if (!strcmp(s, str)) printf("A string \"%s\" e igual a coluna j = %d", str, i+1);
+		if (!strcmp(s, str2)) printf("A string \"%s\" e igual a linha i = %d", str2, i+1);
+	}
+}
+
+void ex10(){
+	int a, b, mmmm;
+//	int mi[mmmm][mmmm] = {{1, 2, 3, 4, 5}, {16, 17, 18, 19, 6}, {15, 24, 25, 20, 7}, {14, 23, 22, 21, 8}, {13, 12, 11, 10, 9}};
+	do {
+		printf("Informe a ordem M da matriz: ");
+		scanf("%d", &mmmm);
+	} while (mmmm > 10 || mmmm < 1);
+	int mi[mmmm][mmmm];
+	for (int i = 0; i < mmmm; i++){
+		for (int j = 0; j < mmmm; j++){
+			scanf("%d", &mi[i][j]);
+		}
+	}
+	int count = 0;
+	printMatrixI(mmmm, mmmm, mi);
+	a = 0; b = mmmm;
+	int inca = 1, i = 0, j = -1;
+	while (count < mmmm*mmmm){
+		for (i = a, j += 1; i < b; i++){
+			if (mi[j][i] != ++count){
+				inca = 0;
+			}
+		}
+		for (j = a + 1, i -= 1; j < b; j++){
+			if (mi[j][i] != ++count){
+				inca = 0;
+			}
+		}
+		for (i = b - 2, j -= 1; i >= a; i--){
+			if (mi[j][i] != ++count){
+				inca = 0;
+			}
+		}
+		for (j = b - 2, i += 1; j >= a+1; j--){
+			if (mi[j][i] != ++count){
+				inca = 0;
+			}
+		}
+		a++; b--;
+	}
+	printf("A matriz %s inca.\n", inca ? "e" : "nao e");
 }
 
 int main(){
 	setlocale(LC_ALL, "");
 	srand(time(NULL));
-	ex8();
+	ex10();
+	ex10();
+	ex10();
+	ex10();
+	ex10();
+	ex10();
 	return 0;
 }
